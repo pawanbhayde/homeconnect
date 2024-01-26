86bc6a97846fd57dca3d4c4f1cb481260611760d
Pawan Bhayde <82137686+pawanbhayde@users.noreply.github.com>
2024-01-26 15:05:29 +0530

    Donation Category Page added

--- lib/widgets/custom_category_card.dart
+++ lib/widgets/custom_category_card.dart
@@ -4,36 +4,40 @@ class HomeCategory extends StatelessWidget {
   const HomeCategory({
     super.key,
     required this.title,
-    required this.image,
+    required this.image, required this.press,
   });
   final String title;
   final String image;
+  final VoidCallback press;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          margin: const EdgeInsets.all(10),
-          width: 60,
-          height: 60,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(500),
-            image: DecorationImage(
-              image: AssetImage(image),
-              fit: BoxFit.cover,
+    return GestureDetector(
+      onTap: press,
+      child: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.all(10),
+            width: 60,
+            height: 60,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(500),
+              image: DecorationImage(
+                image: AssetImage(image),
+                fit: BoxFit.cover,
+              ),
             ),
           ),
-        ),
-        Text(
-          title,
-          style: const TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.w600,
-            color: Colors.black,
+          Text(
+            title,
+            style: const TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
+              color: Colors.black,
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
